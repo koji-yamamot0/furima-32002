dde394b9a9e33df022bdcbcd04b74b5ea481c9dd
koji-yamamot0 <becafo_0515@yahoo.co.jp>
2020-11-19 15:09:06 +0900

    Rubocopの導入, turbolinksの読み込み停止, .Ds_StoreをGitで管理しないように設定

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,7 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("turbolinks").start()
+// require("turbolinks").start() ターボリンクを読み込まないようにコメントアウト
 require("@rails/activestorage").start()
 require("channels")
 
