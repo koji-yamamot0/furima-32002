139d161288197a1568c7c5f49d22ad6299bbb008
koji-yamamot0 <becafo_0515@yahoo.co.jp>
2020-11-24 14:35:01 +0900

    FactoryBotで作成するuserデータを用意

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    family_name           {Faker::Name.unique.name}
+    first_name            {Faker::Name.unique.name}
+    kana_family           {Faker::Name.unique.name}
+    kana_first            {Faker::Name.unique.name}
+    birthday              {1930-01-01}
+  end
+end
