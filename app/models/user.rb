7b8c8196e043b347cd92a32b59288f1aa9bdb264
koji-yamamot0 <becafo_0515@yahoo.co.jp>
2020-11-24 12:37:59 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname,         presence: true
-  validates :family_name,      presence: true
-  validates :first_name,       presence: true
-  validates :kana_family,      presence: true
-  validates :kana_first,       presence: true
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]\z/i.freeze
+  validates_format_of :password, with: VALID_PASSWORD_REGEX, message: 'は英字と数字を含む必要があります'
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]\z/, message: 'は全角で入力してください' } do
+    validates :family_name
+    validates :first_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ン]\z/, message: 'は全角カタカナで入力してください'} do
+    validates :kana_family
+    validates :kana_first
+  end
   validates :birthday,         presence: true
 end
