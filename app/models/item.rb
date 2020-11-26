28d78af19485da92f929f337e1979cc25e9dad3b
koji-yamamot0 <becafo_0515@yahoo.co.jp>
2020-11-26 13:54:12 +0900

    バリデーションの追加、修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,20 +2,23 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  with_options presence: true do
+  with_options presence: { message: 'を入力してください' } do
     validates :title
     validates :text
     validates :price
-    validates :image
+    validates :price
   end
+  validates :image , presence: { message: 'を選択してください'}
 
-  with_options presence: true, numericality: { other_than: 1 } do
-    validates :category
-    validates :delivery_fee
-    validates :item_status
-    validates :ship_from_location
-    validates :shipping_date
+  with_options presence: true, numericality: { other_than: 1 , message: 'を選択してください' } do
+    validates :category_id
+    validates :delivery_fee_id
+    validates :item_status_id
+    validates :ship_from_location_id
+    validates :shipping_date_id
   end
+  validates :price, format: { with: /\A[0-9]\d+\z/,  message: 'は半角数字で入力してください' } ,
+                    inclusion: { in: 300..9999999 , message: 'は指定範囲内に設定してください' }
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
