10bfee737c97021f81030334de6a5e1d574102ff
koji-yamamot0 <becafo_0515@yahoo.co.jp>
2020-11-27 12:48:43 +0900

    @item = Item.find(params[:id])の記述をbefore_actionで省略

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :item_find_params, only: [:show, :edit, :update]
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -19,27 +20,29 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     unless user_signed_in? && @item.user == current_user
       redirect_to action: :index
     end
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
       render :edit
     end
   end
+
   private
 
   def item_params
     params.require(:item).permit(:title, :category_id, :text, :price, :delivery_fee_id, :ship_from_location_id, :shipping_date_id, :item_status_id, :image).merge(user_id: current_user.id)
+  end
+
+  def item_find_params
+    @item = Item.find(params[:id])
   end
 end
