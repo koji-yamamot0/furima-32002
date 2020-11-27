3abc216fd22096171a7f14a124c3409179323081
koji-yamamot0 <becafo_0515@yahoo.co.jp>
2020-11-27 16:00:30 +0900

    マイグレーション

---
+++ db/migrate/20201127065910_create_destinations.rb
@@ -0,0 +1,14 @@
+class CreateDestinations < ActiveRecord::Migration[6.0]
+  def change
+    create_table :destinations do |t|
+      t.string :postal,           null: false
+      t.integer :prefecture_id,   null: false
+      t.string :municipalities,   null: false
+      t.string :address,          null: false
+      t.string :building  
+      t.string :telephone_number, null: false
+      t.references :purchaser,    foreign_key: true
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_25_080910) do
+ActiveRecord::Schema.define(version: 2020_11_27_065910) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", null: false
@@ -33,6 +33,19 @@ ActiveRecord::Schema.define(version: 2020_11_25_080910) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "destinations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "postal", null: false
+    t.integer "prefecture_id", null: false
+    t.string "municipalities", null: false
+    t.string "address", null: false
+    t.string "building"
+    t.string "telephone_number", null: false
+    t.bigint "purchaser_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["purchaser_id"], name: "index_destinations_on_purchaser_id"
+  end
+
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "title", null: false
     t.integer "category_id", null: false
@@ -47,6 +60,15 @@ ActiveRecord::Schema.define(version: 2020_11_25_080910) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
+  create_table "purchasers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "item_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["item_id"], name: "index_purchasers_on_item_id"
+    t.index ["user_id"], name: "index_purchasers_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
@@ -68,5 +90,8 @@ ActiveRecord::Schema.define(version: 2020_11_25_080910) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "destinations", "purchasers"
   add_foreign_key "items", "users"
+  add_foreign_key "purchasers", "items"
+  add_foreign_key "purchasers", "users"
 end
